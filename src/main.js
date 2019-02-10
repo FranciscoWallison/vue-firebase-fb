32faf3321bdc1a0370b5fc85b164d9e8b499a673
Wallison <franciscowallison@gmail.com>
2019-02-10 11:51:49 -0200

    init

--- src/main.js
+++ src/main.js
@@ -9,23 +9,18 @@ let app = ''
 
 // Initialize Firebase criando o projeto Site
 /**
- * Para a simplicidade do tutorial, vamos colocar a configuração Firebase no 
- * arquivo main.js, mas no sistema de produção real, evitar fazer isso e colocá-lo em um 
- * arquivo de configuração específico. Além disso, você pode ver no console alguns avisos do 
- * Firebase sobre apenas importar a parte do SDK que realmente usamos. Na produção, você 
- * definitivamente deveria fazer isso! Mas, novamente, para manter a simplicidade deste 
- * tutorial, pulo esta parte.
- */
-
-let config = {
-  apiKey: "AIzaSyDNj1RmR3XrQu0Xr8U1Ur11MUgF4WSknRE",
-  authDomain: "crafrro.firebaseapp.com",
-  databaseURL: "https://crafrro.firebaseio.com",
-  projectId: "crafrro",
-  storageBucket: "crafrro.appspot.com",
-  messagingSenderId: "772351246272"
-};
-firebase.initializeApp(config);
+ * Adicionar o Firebase ao seu aplicativo da Web
+ * SUAS CONFIGURAÇÕES DO FIREBASE
+ */  
+  var config = {
+    apiKey: "AIzaSyAzi95pn60IA8CH3Knw_AUKrwnLWF3c9Vc",
+    authDomain: "vue-fireabe-fb.firebaseapp.com",
+    databaseURL: "https://vue-fireabe-fb.firebaseio.com",
+    projectId: "vue-fireabe-fb",
+    storageBucket: "vue-fireabe-fb.appspot.com",
+    messagingSenderId: "306576921495"
+  };
+  firebase.initializeApp(config);
 
 /**
  * Agora, inicializamos o aplicativo somente quando tiver certeza de que o 
